52bcf741e7ce961f1aa94d138167ac29fd2e2078
moonhunter <vdcloayza@gmail.com>
2021-07-02 23:31:09 -0400

    aceptando nuevos cambios

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import '../scss/global.scss';
 import 'semantic-ui-css/semantic.min.css';
-=======
-import '../styles/globals.css'
->>>>>>> 08a847242e749cb27bb9abbc621bed4d6ac1d33a
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
